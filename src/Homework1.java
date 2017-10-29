c2820f9ba7d6f147f67608cabddae0fa4eef1ca2
李晓冬 <lixiao1k@163.com>
2017-10-29 11:25:25 +0800

    完成

--- src/Homework1.java
+++ src/Homework1.java
@@ -26,7 +26,7 @@ public class Homework1 {
         readAllocationFile();
     }
 
-    public void createTables(){
+    private void createTables(){
         String student_dropifexists = "drop table if exists student;";
         String student_createSql = "create table student" +
                 "(" +
@@ -74,7 +74,7 @@ public class Homework1 {
     }
 
 
-    public void insertStudentData(){
+    private void insertStudentData(){
         ArrayList<String[]> datalist = allocationData;
         Iterator<String[]> iterator = datalist.iterator();
         String insertSql = "insert into student(sid,sname,gender,department) values (?,?,?,?)";
@@ -88,7 +88,7 @@ public class Homework1 {
                 preStatement.setString(3,rowdata[3]);
                 preStatement.setString(4,rowdata[0]);
                 preStatement.addBatch();
-            }
+            }//批量处理，提高效率
             preStatement.executeBatch();
             conn.commit();
         } catch (SQLException e) {
@@ -101,7 +101,7 @@ public class Homework1 {
         }
     }
 
-    public void insertDormitoryData(){
+    private void insertDormitoryData(){
         String insertSql = "insert into dormitory(dname,telephone) values (?,?)";
         ArrayList<String[]> phonelist = getPhoneData();
         try {
@@ -150,7 +150,7 @@ public class Homework1 {
         }
     }
 
-    public void insertAccommodationData(){
+    private void insertAccommodationData(){
         ArrayList<String[]> accomData = allocationData;
         String insertSql = "insert into accommodation(sid,dname) values (?,?)";
         try {
@@ -199,8 +199,8 @@ public class Homework1 {
         }
         return list;
     }
-
-    public void readAllocationFile() {
+    //读分配信息的excel文件内容
+    private void readAllocationFile() {
         String path = "resource/分配方案.xls";
         File file = new File(path);
         InputStream is = null;
@@ -226,7 +226,7 @@ public class Homework1 {
         }
         allocationData = filedata;
     }
-
+    // 获取excel文件一行的数据
     private String[] getAllocationDataFromRow(HSSFRow row){
         String [] values = new String[7];
         for(int index = 0; index < 7; index++){
@@ -235,7 +235,7 @@ public class Homework1 {
         return values;
 
     }
-
+    //获取excel文件单元格中的数据
     private String getCellValue(HSSFCell cell){
         if(cell.getCellType() == cell.CELL_TYPE_BOOLEAN){
             return String.valueOf(cell.getBooleanCellValue());
@@ -259,4 +259,123 @@ public class Homework1 {
         return connection;
     }
 
+    //查询"王小星"同学所在宿舍楼的所有院系。
+    private void getDepartWithWang(){
+        String sql = "select distinct s.department from student s, accommodation a where s.sid = a.sid " +
+                "and a.dname = (select a1.dname from student s1, accommodation a1 where s1.sid = a1.sid" +
+                " and s1.sname = '王小星')";
+        try {
+            Statement statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()){
+                System.out.println(resultSet.getString(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            try {
+                conn.rollback();
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+    //将陶园1舍的住宿费提高到1200元
+    private void updateFare(){
+        String sql = "update dormitory set fare = 1200 where dname = '陶园1舍'";
+        try {
+            Statement statement = conn.createStatement();
+            statement.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            try {
+                conn.rollback();
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
+        }
+
+
+    }
+    //交换软件学院男女生宿舍
+    private void exchange(){
+        String manDorSql = "SELECT DISTINCT a.dname FROM student s, dormitory d,accommodation a WHERE s.sid = a.sid AND d.dname = a.dname " +
+                "AND s.department = '软件学院' AND s.gender = '男'";
+        String womenDorSql = "SELECT DISTINCT a.dname FROM student s, dormitory d,accommodation a WHERE s.sid = a.sid AND d.dname = a.dname\n" +
+                "AND s.department = '软件学院' AND s.gender = '女'";
+        String updateManDorSql = "UPDATE accommodation set dname = ? " +
+                "WHERE sid IN (SELECT s.sid FROM student s WHERE s.department = '软件学院' AND s.gender = '男')";
+        String updateWomenSql = "UPDATE accommodation set dname = ? " +
+                "WHERE sid IN (SELECT s.sid FROM student s WHERE s.department = '软件学院' AND s.gender = '女')";
+        try {
+            Statement statement = conn.createStatement();
+            ResultSet manDor = statement.executeQuery(manDorSql);
+            manDor.next();
+            String manDorString = manDor.getString(1);
+            System.out.println(manDorString);
+            ResultSet womenDor = statement.executeQuery(womenDorSql);
+            womenDor.next();
+            String womenDorString = womenDor.getString(1);
+            System.out.println(womenDorString);
+            PreparedStatement preparedStatement = conn.prepareStatement(updateManDorSql);
+            preparedStatement.setString(1,womenDorString);
+            preparedStatement.addBatch();
+            preparedStatement.executeBatch();
+            conn.commit();
+            preparedStatement = conn.prepareStatement(updateWomenSql);
+            preparedStatement.setString(1,manDorString);
+            preparedStatement.addBatch();
+            preparedStatement.executeBatch();
+            conn.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            try {
+                conn.rollback();
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+
+
+    public static void main(String[] args){
+        Homework1 homework1 = new Homework1();
+        long start = 0;
+        long end = 0;
+        System.out.println("开始为表student插入数据...");
+        start = System.currentTimeMillis();
+        homework1.insertStudentData();
+        end = System.currentTimeMillis();
+        System.out.println("表student数据插入完成...用时"+(end-start)+"ms");
+        System.out.println("开始为表dormitory插入数据...");
+        start = System.currentTimeMillis();
+        homework1.insertDormitoryData();
+        end = System.currentTimeMillis();
+        System.out.println("表dormitory数据插入完成...用时"+(end-start)+"ms");
+        System.out.println("开始为表accomodation插入数据...");
+        start = System.currentTimeMillis();
+        homework1.insertAccommodationData();
+        end = System.currentTimeMillis();
+        System.out.println("表accomodation数据插入完成...用时"+(end-start)+"ms");
+        System.out.println("开始为查询王小星所在楼的所有院系...");
+        start = System.currentTimeMillis();
+        homework1.getDepartWithWang();
+        end = System.currentTimeMillis();
+        System.out.println("查询完成"+(end-start)+"ms");
+        System.out.println("开始更新宿舍费用...");
+        start = System.currentTimeMillis();
+        homework1.updateFare();
+        end = System.currentTimeMillis();
+        System.out.println("更新完成"+(end-start)+"ms");
+        System.out.println("开始交换宿舍...");
+        start = System.currentTimeMillis();
+        homework1.exchange();
+        end = System.currentTimeMillis();
+        System.out.println("交换完成"+(end-start)+"ms");
+        try {
+            homework1.conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
